38e60c7e72d5ed1f3d3f4e0cc5500c7cb961a477
Shah, Shail <Shail.Shah@garmin.com>
2020-10-21 11:32:29 -0500

    New A2A Changes
    Issue: CA-89143

--- app/src/main/java/com/fitpay/issuerdemo/MainActivity.java
+++ app/src/main/java/com/fitpay/issuerdemo/MainActivity.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,8 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.etPassword)
     EditText etPassword;
+    @BindView(R.id.issuer_list)
+    Spinner issuer_list;
+    @BindView(R.id.response_type)
+    Spinner response_type;
+    @BindView(R.id.request)
+    TextView request;
 
     private boolean canSkipWithSuccess;
+
+    private static String VISA_AUTH_CODE_KEY = "";
+    private static String VISA_RESPONSE_KEY = "";
+    private static String MASTER_CARD_AUTH_CODE_KEY = "";
+    private static String MASTER_CARD_RESPONSE_KEY = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +39,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        // setup spinners
+        setupSpinners();
+
         if (getIntent() != null && getIntent().getAction() != null) {
             switch (getIntent().getAction()) {
                 case "com.fitpay.issuerdemo.generate_auth_code":
+                    request.setText(getIntent().getAction());
                     //submit password
+                    etPassword.setVisibility(View.VISIBLE);
                     break;
                 case "com.fitpay.issuerdemo.authenticate_user":
+                    request.setText(getIntent().getAction());
                     canSkipWithSuccess = true;
+                    // automatic flow, hide input text
+                    etPassword.setVisibility(View.INVISIBLE);
                     break;
                 default:
                     //launched manually
@@ -37,28 +61,63 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    void setupSpinners() {
+        String[] issuerItems = new String[]{"VISA", "MASTERCARD", "MAESTRO"};
+        String[] responseItems = new String[]{"Approved", "Declined", "Failure"};
+
+        ArrayAdapter<String> issuerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, issuerItems);
+        issuer_list.setAdapter(issuerAdapter);
+        ArrayAdapter<String> responseAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, responseItems);
+        response_type.setAdapter(responseAdapter);
+    }
+
     @OnClick(R.id.btnSubmit)
     void submit() {
+        if (canSkipWithSuccess) {
+            handleAutomaticFlow();
+            return;
+        }
+
+        handleAuthCodeFlow();
+    }
+
+    private void handleAutomaticFlow() {
+        String selectedResponse = (String) response_type.getSelectedItem();
+        switch (selectedResponse) {
+            case "Approved":
+                finishActivity("approved", null);
+                break;
+            case "Declined":
+                finishActivity("declined", null);
+                break;
+            case "Failure":
+                finishActivity("failure", null);
+                break;
+        }
+    }
+
+    private void handleAuthCodeFlow() {
         String password = etPassword.getText().toString();
         if (!TextUtils.isEmpty(password)) {
-            if (canSkipWithSuccess) {
-                finishActivity("approved", null);
-                return;
-            }
+
 
             String authResponse = "approved";
             String authCode = null;
 
-            String lastStr = password.substring(password.length() - 1);
-            switch (lastStr) {
-                case "d":
+            String response = (String) response_type.getSelectedItem();
+            switch (response) {
+                case "Approved":
+                    authResponse = "approved";
+                    authCode = password;
+                    break;
+
+                case "Declined":
                     authResponse = "declined";
                     break;
-                case "f":
+
+                case "Failure":
                     authResponse = "failure";
                     break;
-                default:
-                    authCode = password;
             }
 
             finishActivity(authResponse, authCode);
@@ -76,9 +135,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void finishActivity(String authResponse, String authCode) {
         Intent result = new Intent();
-        result.putExtra("STEP_UP_RESPONSE", authResponse);
-        if (authCode != null) {
-            result.putExtra("STEP_UP_AUTH_CODE", authCode);
+
+        String issuerType = (String) issuer_list.getSelectedItem();
+
+        if (issuerType.equals("VISA")) {
+            result.putExtra("STEP_UP_RESPONSE", authResponse);
+            if (authCode != null) {
+                result.putExtra("STEP_UP_AUTH_CODE", authCode);
+            }
+        } else {
+            result.putExtra("issuerMobileAppAuthResponse", authResponse);
+            if (authCode != null) {
+                result.putExtra("TAV", authCode);
+            }
         }
         setResult(RESULT_OK, result);
         finish();
